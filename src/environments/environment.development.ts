cdd2f05c8af9da39175ad1855094f6d906fc67ee
Antonio Kobashikawa <akobashikawa@gmail.com>
2026-07-30 18:32:49 -0500

    Agregar archivo de entorno de desarrollo y actualizar configuraciones de reemplazo de archivos

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://royal-escolar-api.rsdev.site/api/v1',
+  apiUrl: 'http://localhost:3000/api/v1',
   appName: 'EscolarERP',
   version: '1.0.0',
   tokenExpirationWarning: 300,
